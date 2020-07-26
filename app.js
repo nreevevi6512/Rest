0f356d2a5d0205bc29e3bce1d423c7b64cc5f2b5
nreevevi6512 <68641693+nreevevi6512@users.noreply.github.com>
2020-07-27 07:57:47 +0800

    about

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("hello modafaka");
 })
+
+app.get("/about", (req, res) => {
+    res.send("Stupid fucker");
+})
 //Server
 let port = 22422;
 app.listen(port, function () {
